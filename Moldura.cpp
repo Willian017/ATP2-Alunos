2027b1c962e5a3d0dff6803ed1794c945d343a97
willian017 <willianyuji100@gmail.com>
2024-10-03 00:16:01 -0300

    Update Moldura

--- Moldura.cpp
+++ Moldura.cpp
@@ -29,23 +29,22 @@ void Moldura(int CI, int LI, int CF, int LF, int CorT, int CorF)
 
 int main ()
 {
-	i=7;
+	int i=9;
 	Moldura(1,1,80,25,1,7);
 	Moldura(2,2,79,4,1,3);
 	gotoxy(33,3);
 	printf("ATP 2 Alunos");
-	Moldura(2,5,30,24,1,3);
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	gotoxy(3,i++);
-	printf("[A] Cad Alunos\n");
-	printf("\n");
+	Moldura(2,5,79,24,1,3);
+	gotoxy(37,7);
+	printf("Menu")
+	gotoxy(5,i++); printf("[A] Cadastro de Alunos");gotoxy(45,i); printf("[I] Excluir Notas");
+	gotoxy(5,i++); printf("[B] Cadastro de Disciplinas");gotoxy(45,i); printf("[J] Visualizar Alunos");
+	gotoxy(5,i++); printf("[C] Cadastro de Notas");gotoxy(45,i); printf("[K] Visualizar Disciplina");
+	gotoxy(5,i++); printf("[D] Atualizar Alunos");gotoxy(45,i); printf("[L] Visualizar Notas");
+	gotoxy(5,i++); printf("[E] Atualizar Disciplinas");gotoxy(45,i); printf("[M] Reprovado em 2 ou mais disciplinas");
+	gotoxy(5,i++); printf("[F] Atualizar Notas");gotoxy(45,i); printf("[N] Visualizar Alunos por Letras");
+	gotoxy(5,i++); printf("[G] Excluir Alunos");gotoxy(45,i); printf("[O] Visualizar Disciplinas com media abaixo de 6.0");
+	gotoxy(5,i++); printf("[H] Excluir Disciplinas");gotoxy(45,i); printf("[P] Visualizar Todosa os Dados");
+	gotoxy(5,i++); printf("[ESC] Sair\n");
+    gotoxy(5,i+=2); printf("Opcao desejada: ");
 }
