469f373eac215f487376da32a51ab32b4248fb45
willian017 <willianyuji100@gmail.com>
2024-10-03 18:07:44 -0300

    Update main

---
+++ Testes.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio2.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <windows.h>
+
+struct TpAluno
+{
+    char Nome[30], RA[13];
+};
+
+struct TpDisci
+{
+    char Descr[30];
+
+    int CodDisc;
+};
+
+struct TpNota
+{
+    char RA[13];
+
+    int CodDisc;
+
+    float Nota;
+};
+
+int main()
+{
+
+}
